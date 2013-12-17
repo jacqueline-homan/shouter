2ab820dd1367316cf55e55f0c9350e1fafcf97d6
jacqueline-homan <jacquelinehoman7@gmail.com>
2013-12-17 13:37:47 -0500

    added route for dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
+  before_filter :check_logged_in_user
   def show
   end
+
+  private
+
+  def check_logged_user
+    if signed_in?
+      redirect_to dashboard_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shouter::Application.routes.draw do
   root to: 'home#show', via: :get
+  resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   
